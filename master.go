89c06a3b5462192419be1de1afc00333145eb7ee
mathewtapilatu <mathewtapilatu1@gmail.com>
2021-08-05 01:57:24 +0700

    resolve conflict

--- master.go
+++ master.go
@@ -4,9 +4,7 @@ import "fmt"
 
 func main() {
 	fmt.Println("push to master")
-<<<<<<< HEAD
-	fmt.Println("featureA")
-=======
+	fmt.Println("push to featureA")
 	fmt.Println("push to featureB")
->>>>>>> featureB
+
 }
